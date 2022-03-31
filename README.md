a41a83d66e33dff75e5001de3c18c039b5deb3ea
Crucix <fchang1720@gmail.com>
2022-03-31 18:19:27 -0500

    updated readme, added function to save input text

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Work-Day-Scheduler
+
+In this project, I create a scheduler that helps plan out activities for the day.
+Not only is the time and date displayed, information can also be stored in each of the time blocks.
+
+Upon page refresh, the information in each time block does not disappear. As time passes throughout the day, each time block will change its background color to mark if it is in the past, present, or future. The time block color will be grey (if in the past), red (if displaying time in the current hour), or green (if the time has yet to arrive at that time block). After the last time block has expired, the colors will automatically reset to white and will change color accordingly again the next day. 
+
+![Here is a screenshot of my project in action](./assets/Work%20Day%20Scheduler.jpg)
--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,14 @@
 // Setting variables
 var timeDisplayEl = $('#time-display');
 var saveBtn1 = $(".saveBtn1");
+var saveBtn2 = $(".saveBtn2");
+var saveBtn3 = $(".saveBtn3");
+var saveBtn4 = $(".saveBtn4");
+var saveBtn5 = $(".saveBtn5");
+var saveBtn6 = $(".saveBtn6");
+var saveBtn7 = $(".saveBtn7");
+var saveBtn8 = $(".saveBtn8");
+var saveBtn9 = $(".saveBtn9");
 var taskNineInput = $("#task9");
 var taskTenInput = $("#task10");
 var taskElevenInput = $("#task11");
@@ -23,7 +31,7 @@ function displayTime() {
 function setColor() {
     var b = new Date();
     var bNow = b.getHours();
-    if (bNow >= 9 && bNow < 10) {
+    if (bNow >= 9 && bNow < 18) {
         taskNineInput.addClass("present");
         taskTenInput.addClass("future");
         taskElevenInput.addClass("future");
@@ -34,43 +42,43 @@ function setColor() {
         taskFourInput.addClass("future");
         taskFiveInput.addClass("future");
     }
-    if (bNow >=10 && bNow <11) {
+    if (bNow >=10 && bNow <18) {
         taskNineInput.removeClass("present");
         taskNineInput.addClass("past");
         taskTenInput.removeClass("future");
         taskTenInput.addClass("present");
     }
-    if (bNow >=11 && bNow <12) {
+    if (bNow >=11 && bNow <18) {
         taskTenInput.removeClass("present");
         taskTenInput.addClass("past");
         taskElevenInput.removeClass("future");
         taskElevenInput.addClass("present");
     }
-    if (bNow >=12 && bNow <13) {
+    if (bNow >=12 && bNow <18) {
         taskElevenInput.removeClass("present");
         taskElevenInput.addClass("past");
         taskTwelveInput.removeClass("future");
         taskTwelveInput.addClass("present");
     }
-    if (bNow >=13 && bNow <14) {
+    if (bNow >=13 && bNow <18) {
         taskTwelveInput.removeClass("present");
         taskTwelveInput.addClass("past");
         taskOneInput.removeClass("future");
         taskOneInput.addClass("present");
     }
-    if (bNow >=14 && bNow <15) {
+    if (bNow >=14 && bNow <18) {
         taskOneInput.removeClass("present");
         taskOneInput.addClass("past");
         taskTwoInput.removeClass("future");
         taskTwoInput.addClass("present");
     }
-    if (bNow >=15 && bNow <16) {
+    if (bNow >=15 && bNow <18) {
         taskTwoInput.removeClass("present");
         taskTwoInput.addClass("past");
         taskThreeInput.removeClass("future");
         taskThreeInput.addClass("present");
     }
-    if (bNow >=16 && bNow <17) {
+    if (bNow >=16 && bNow <18) {
         taskThreeInput.removeClass("present");
         taskThreeInput.addClass("past");
         taskFourInput.removeClass("future");
@@ -82,32 +90,65 @@ function setColor() {
         taskFiveInput.removeClass("future");
         taskFiveInput.addClass("present");
     }
-    if (bNow >=18 && bnow <19) {
-        taskNineInput.removeClass("past");
-        taskTenInput.removeClass("past");
-        taskElevenInput.removeClass("past");
-        taskTwelveInput.removeClass("past");
-        taskOneInput.removeClass("past");
-        taskTwoInput.removeClass("past");
-        taskThreeInput.removeClass("past");
-        taskFourInput.removeClass("past");
-        taskFiveInput.removeClass("present");
-    }
-  
 }
 setColor();
 
-// function set9() {
-//     var nineDisplay = localStorage.getItem("task 9");
-//     if (lastGrade !== null) {
-//     document.getElementById("#task9").innerHTML = nineDisplay
-//     }
-    
-// }
+// function that saves values in input text.
+function setValues() {
+    $("#task9").val(localStorage.getItem("task 9"))
+    $("#task10").val(localStorage.getItem("task 10"))
+    $("#task11").val(localStorage.getItem("task 11"))
+    $("#task12").val(localStorage.getItem("task 12"))
+    $("#task1").val(localStorage.getItem("task 1"))
+    $("#task2").val(localStorage.getItem("task 2"))
+    $("#task3").val(localStorage.getItem("task 3"))
+    $("#task4").val(localStorage.getItem("task 4"))
+    $("#task5").val(localStorage.getItem("task 5"))
+}
+setValues();
+
 // event listener to save to local storage
 saveBtn1.on('click', function(e) {
     e.preventDefault();
-    
     localStorage.setItem("task 9", taskNineInput.val())
-    // set9();
+});
+
+saveBtn2.on('click', function(e) {
+    e.preventDefault();
+    localStorage.setItem("task 10", taskTenInput.val())
+});
+
+saveBtn3.on('click', function(e) {
+    e.preventDefault();
+    localStorage.setItem("task 11", taskElevenInput.val())
+});
+
+saveBtn4.on('click', function(e) {
+    e.preventDefault();
+    localStorage.setItem("task 12", taskTwelveInput.val())
+});
+
+saveBtn5.on('click', function(e) {
+    e.preventDefault();
+    localStorage.setItem("task 1", taskOneInput.val())
+});
+
+saveBtn6.on('click', function(e) {
+    e.preventDefault();
+    localStorage.setItem("task 2", taskTwoInput.val())
+});
+
+saveBtn7.on('click', function(e) {
+    e.preventDefault();
+    localStorage.setItem("task 3", taskThreeInput.val())
+});
+
+saveBtn8.on('click', function(e) {
+    e.preventDefault();
+    localStorage.setItem("task 4", taskFourInput.val())
+});
+
+saveBtn9.on('click', function(e) {
+    e.preventDefault();
+    localStorage.setItem("task 5", taskFiveInput.val())
 });
